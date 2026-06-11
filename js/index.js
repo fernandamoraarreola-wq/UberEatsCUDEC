89a8e058c762ac0a6dc4d75e771668e357407a52
fernandamoraarreola-aq <fernandamoraarreola@gmail.com>
2026-06-10 21:26:21 -0600

    modificacion de precio automatico y agregacion de platillos

--- js/index.js
+++ js/index.js
@@ -9,12 +9,10 @@ document.addEventListener('DOMContentLoaded', function() {
   M.Sidenav.init(forms, {edge: 'left'});
 });
 
-btnAgregarPlatillo .addEventListener('click', function (){
-    alert('Platillo agregado')
-});
 
 function mostrarPlatillo(platillo,id){
-  contenido += `<div class="card-panel recipe white row" data-id="${id}">
+  contenido += `<div class="card-panel recipe white row" 
+  id="${id}">
   <div class="recipe-details">
        <div class ="recipe-title">
           ${platillo.nombre}
@@ -32,3 +30,12 @@ function mostrarPlatillo(platillo,id){
     </div>`;
   document.querySelector('.recipes').innerHTML = contenido;
 }
+function actualizarPlatillo(platillo,id){
+  let tarjeta = document.getElementById(`${id}`);
+  tarjeta.querySelector(".recipe-title").innerHTML = platillo.nombre;
+  tarjeta.querySelector(".recipe-ingredients").innerHTML = platillo.ingredientes;
+   tarjeta.querySelector(".recipe-price").innerHTML = platillo.precio;
+  
+}
+
+
